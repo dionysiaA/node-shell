39cba53bdcd3b84262b55693c72721c4dec1ba47
Ben McCain <mccain.ben2@gmail.com>
2017-03-06 22:10:32 -0500

    complete no piping

--- bash.js
+++ bash.js
@@ -1,16 +1,19 @@
 var userCommand = require('./commands')
 // Output a prompt
-process.stdout.write('prompt > ');
-
+const prompt = '\n prompt > ';
+process.stdout.write(prompt);
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
   var cmd = data.toString().trim().split(" ");
 
   var input = cmd.slice(1);
   if(userCommand[cmd[0]]){
-    userCommand[cmd[0]](input);
+    userCommand[cmd[0]](input,done);
   }
-  else process.stderr.write("command not found: " + cmd);
-
-  process.stdout.write('\nprompt > ');
+  else process.stderr.write("command not found: " + cmd + prompt);
 });
+
+
+function done(output){
+  process.stdout.write(output + prompt);
+}
--- commands.js
+++ commands.js
@@ -1,31 +1,31 @@
-var fs = require("fs");
-var readline = require('readline');
+var fs = require('fs');
+var request = require('request');
 
-function pwd(){
+function pwd(input,done){
   // console.log(process.argv[1]);
-  process.stdout.write(process.cwd());
+  done(process.cwd());
 }
 
-function date(){
-  process.stdout.write(Date());
+function date(input,done){
+  done(Date());
 }
 
-function ls(){
+function ls(input,done){
   fs.readdir('.', function(err, files) {
     if(err) throw err;
-    process.stdout.write(files.join("\n"));
+    done(files.join("\n"));
 });
 }
 
-function echo(input){
+function echo(input,done){
   // process.stdout.write(input.join(" "));
   var output = input.map(function(arg){
     return arg[0] === "$" ? process.env[arg.slice(1)] : arg;
   }).join(" ");
-  process.stdout.write(output);
+  done(output);
 }
 
-function cat(input){
+function cat(input,done){
   console.log(input);
   var texts = [];
   var count = 0;
@@ -34,25 +34,67 @@ function cat(input){
      if(err) throw err;
      texts[i] = text;
      count ++
-     if(count === input.length) process.stdout.write(texts.join(""))
+     if(count === input.length) done(texts.join(""));
    })
  })
 }
 
-function head(input) {
+function head(input,done) {
   input = input[0];
-  fs.readFile(input, {enconding: "utf8"}, function(err,text){
+  fs.readFile(input, {encoding: "utf8"}, function(err,text){
     if(err) throw err;
-    process.stdout.write(text.toString().split('\n').slice(0,5).join('\n'));
+    done(text.toString().split('\n').slice(0,5).join('\n'));
   })
 }
 
-function tail(input) {
+function tail(input,done) {
   input = input[0];
-  fs.readFile(input,{enconding: "utf8"}, function(err,text){
+  fs.readFile(input,{encoding: "utf8"}, function(err,text){
     if(err) throw err;
-    process.stdout.write(text.toString().split("\n").slice(-5).join("\n"));
-  })
+    done(text.toString().split("\n").slice(-5).join("\n"));
+  });
+}
+
+function sort(input,done) {
+  input = input[0];
+  fs.readFile(input, {encoding: "utf8"}, function(err,text){
+    if(err) throw err;
+    done(text.toString().split('\n').sort().join('\n'));
+  });
+}
+
+function wc(input,done) {
+  input = input[0];
+  fs.readFile(input, {encoding: "utf8"}, function(err,text){
+    if(err) throw err;
+    done(text.toString().split('\n').length);
+  });
+}
+
+function uniq(input,done) {
+  input = input[0];
+  fs.readFile(input, {encoding: "utf8"}, function(err,text){
+    if(err) throw err;
+    var lines = text.toString().split('\n');
+    for(var i = 0;i < lines.length;i++){
+      if(lines[i] === lines[i + 1]){
+        lines.splice(i,1);
+        i--;
+      }
+    }
+    done(lines.join('\n'));
+  });
+}
+
+function curl(url,done){
+  url = url[0];
+  if(url.slice(0,7) !== "http://") url = "http://" + url;
+  request(url, function (err, response, body){
+    if(err) throw err;
+    else if(response && (response.statusCode > 399)) throw new Error(response.statusCode);
+    if(body) done(body);
+    else done('');
+  });
 }
 
 module.exports = {
@@ -62,5 +104,9 @@ module.exports = {
   echo: echo,
   cat: cat,
   head: head,
-  tail: tail
+  tail: tail,
+  sort: sort,
+  wc: wc,
+  uniq: uniq,
+  curl: curl
 }
